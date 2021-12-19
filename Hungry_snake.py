f505fede2ac5ee142e170753d94596ec5e255190
xenya <kakopsova@miem.hse.ru>
2021-12-19 17:40:54 +0300

    Initial commit

--- Hungry_snake.py
+++ Hungry_snake.py
@@ -2,11 +2,34 @@ import pygame
 import time
 import random
 
+def menu(choice):
+    """Функция используется для изменения статуса игры
+    Принимает на  вход choice - выбор, введенный игроком с клавиатуры
+    Возвращает game_over - статус игры, game_close - статус открытия игры"""
+    if choice == pygame.K_q:
+        game_over = True
+        game_close = False
+    if choice == pygame.K_n:
+        game_process()
+    return game_over, game_close
+
+def border(x, y):
+    """Функция используется для определения, находится ли змея в границах игры
+    Принимает на вход значения x - текущая координата змейки по оси X
+    y - текущая координаат змейки по оси Y
+    Возвращает game_close - статус игры (активен / неактивен)"""
+    if x >= 525 or x < 65 or y >= 525 or y < 65:
+        game_close = True
+    else:
+        game_close = False
+
+    return game_close
 
 def moving(dirct, snake_size):
-    """Функция используется для передвижения змеи. Принимает на вход переменные - dirict, отвечающую
-    за направление движения, и snake_size, отвечающую за размер одного блока змеи. Возвращает значения,
-    в каком напралении сдвинулась змея по оси X и Y"""
+    """Функция используется для передвижения змеи.
+    Принимает на вход переменные - dirict, отвечающую за направление движения,
+    и snake_size, отвечающую за размер одного блока змеи.
+    Возвращает значения, в каком напралении сдвинулась змея по оси X (dx) и Y (dy)"""
     dx = 0
     dy = 0
     left = pygame.K_LEFT
@@ -27,35 +50,65 @@ def moving(dirct, snake_size):
         dx = 0
     return dx, dy
 
-"""Функция испоьзуется для вывода количества очков. 
-Принимает на вход кол-во очков"""
+def get_food(x, y, x_food, y_food, snake_len, snake_size):
+    """Функция используется для определения попала ли змейка на еду
+    Принимает на вход: x - координата головы зммеи по X, y - координата головы змеи по Y,
+     x_food - координата еды по X, y_food - координата еды по Y, snake_len - длина змеи
+     snake_size - размер одного блока змейки
+     Возвращает snake_len - длину змеи после определения попала ли змея на еду,
+     x_food - координата еды по X после определения попала ли змея на еду,
+     y_food - координата еды по Y после определения попала ли змея на еду"""
+    if x == x_food and y == y_food:
+        x_food = round(random.randrange(65, width - snake_size - 75) / 10.0) * 10.0
+        y_food = round(random.randrange(65, height - snake_size - 75) / 10.0) * 10.0
+        snake_len += 1
+    return snake_len, x_food, y_food
+
 def score_change(score):
+    """Функция испоьзуется для вывода количества очков.
+    Принимает на вход кол-во очков"""
     score_font = pygame.font.SysFont("comicsans", 35)
     value = score_font.render("Score: " + str(score), True, score_color)
     screen.blit(value, [0, 0])
-    return score
 
-"""Функция используется для вывода змеи. 
-Принимает на вход размер одного блока змейки и ее длинну"""
+
+def print_level(level):
+    """Функция для вывода уровня игры
+    Принимает на вход level - значение текущего уровня"""
+    level_font = pygame.font.SysFont("comicsans", 35)
+    value = level_font.render("Level: " + str(level), True, white)
+    screen.blit(value, [420, 0])
+
 def snake_change(snake_size, snake_list):
+    """Функция используется для вывода змеи.
+    Принимает на вход размер одного блока змейки и ее длинну"""
     for a in snake_list:
         pygame.draw.rect(screen, snake_color, [a[0], a[1], snake_size, snake_size])
 
-"""Функция предназначена для вывода сообщений в игре. 
-На вход принимает само сообщение, его цвет, координаат по X и Y"""
 def message(msg, color, size_x, size_y):
+    """Функция предназначена для вывода сообщений в игре.
+    На вход принимает само сообщение, его цвет, координаат по X и Y"""
     font_style = pygame.font.SysFont("bahnschrift", 25)
     mesg = font_style.render(msg, True, color)
     screen.blit(mesg, [size_x, size_y])
 
-"""Функция отвечает за процесс игры"""
+def higher_level(snake_len, level, snake_speed):
+    """Функция предназначена для повышения уровня игры/скорости змейки
+    Принимает на вход snake_len - длину змеи, level - текущий уровень игры,
+    snake_speed - скорсть змейки
+    Возвращает level - уровень, snake_speed - скорость змейки"""
+    if snake_len > level * 10 + 1:
+        snake_speed += 1
+        level += 1
+    return level, snake_speed
+
+
 def game_process():
+    """Функция отвечает за процесс игры"""
     pygame.init()
 
     screen = pygame.display.set_mode((width, height))
     pygame.display.set_caption('Hungry snake')
-
-    score = 0
 
     game_over = False
     game_close = False
@@ -89,11 +142,10 @@ def game_process():
 
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_q:
-                        game_over = True
-                        game_close = False
-                    if event.key == pygame.K_n:
-                        game_process()
+                    choice = event.key
+                    game_over, game_close = menu(choice)
+
+
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -101,10 +153,11 @@ def game_process():
             if event.type == pygame.KEYDOWN:
                 x_change, y_change = moving(event.key, snake_size)
 
-        if x >= 525 or x < 70 or y >= 525 or y < 70:
-            game_close = True
+        game_close = border(x, y)
+
         x += x_change
         y += y_change
+
         screen.fill(screen_color)
         frane = pygame.draw.rect(screen, white, [50, 50, 500, 500])
         screent = pygame.draw.rect(screen, screen_color, [55, 55, 490, 490])
@@ -113,6 +166,7 @@ def game_process():
         snake_head.append(x)
         snake_head.append(y)
         snake_list.append(snake_head)
+
         if len(snake_list) > snake_len:
             del snake_list[0]
 
@@ -121,21 +175,18 @@ def game_process():
                 game_close = True
 
         snake_change(snake_size, snake_list)
-        score_change(score)
+        score_change(snake_len - 1)
+        print_level(level)
 
         pygame.display.update()
 
-        if x == x_food and y == y_food:
-            x_food = round(random.randrange(65, width - snake_size - 75) / 10.0) * 10.0
-            y_food = round(random.randrange(65, height - snake_size - 75) / 10.0) * 10.0
-            snake_len += 1
-            score = score_change(snake_len - 1)
+        snake_len, x_food, y_food = get_food(x, y, x_food, y_food, snake_len, snake_size)
 
         timing.tick(snake_speed)
 
-        if snake_len - 1 >= level * 10:
-            snake_speed += 1
-            level += 1
+        level, snake_speed = higher_level(snake_len, level, snake_speed)
+
+
 
     screen.fill(screen_color)
     message("GAME OVER", mesg_color, 230, 220)
@@ -144,7 +195,6 @@ def game_process():
 
     pygame.quit()
     quit()
-
 
 white = (255, 255, 255)
 score_color = (0, 250, 154)
@@ -155,6 +205,7 @@ screen_color = (72, 61, 139)
 
 width = 600
 height = 600
+
 if __name__ == "__main__":
     screen = pygame.display.set_mode((width, height))
     pygame.display.set_caption('Hungry snake')
--- tests.py
+++ tests.py
@@ -1,14 +1,13 @@
-import Hungry_snake
-from Hungry_snake import moving
-import unittest
-import pygame
 import unittest.mock
-"""
-class Screen(pygame.Surface):
-    blit = unittest.mock.MagicMock()
 
-screen = Screen((600,600))
-"""
+import pygame
+
+from Hungry_snake import border
+from Hungry_snake import get_food
+from Hungry_snake import higher_level
+from Hungry_snake import moving
+from Hungry_snake import menu
+
 class TestSnake(unittest.TestCase):
 
     def test_moving(self):
@@ -25,6 +24,46 @@ class TestSnake(unittest.TestCase):
         dirct = pygame.K_RIGHT
         self.assertEqual(moving(pygame.K_UP, snake_size), (0, -20))
 
-    def test_score(self):
+    def test_getfood(self):
+        snake_size = 10
         snake_len = 10
-        self.assertEqual(Hungry_snake.score_change(snake_len - 1), 9)
+        x = 150
+        y = 150
+        x_food = 120
+        y_food = 115
+        self.assertEqual(get_food(x, y, x_food, y_food, snake_len, snake_size), (10, x_food, y_food))
+        snake_len = 20
+        snake_size = 10
+        x = 100
+        y = 155
+        x_food = 100
+        y_food = 155
+        self.assertNotEqual(get_food(x, y, x_food, y_food, snake_len, snake_size), (20, 100, 155))
+
+    def test_boreder(self):
+        x = 520
+        y = 500
+        self.assertEqual(border(x, y), False)
+        x = 530
+        y = 290
+        self.assertEqual(border(x, y), True)
+        x = 500
+        y = 60
+        self.assertEqual(border(x, y), True)
+        x = 20
+        y = 570
+        self.assertEqual(border(x, y), True)
+
+    def test_level(self):
+        snake_len = 12
+        level = 1
+        snake_speed = 10
+        self.assertEqual(higher_level(snake_len, level, snake_speed), (2, 11))
+        snake_len = 19
+        level = 2
+        snake_speed = 10
+        self.assertEqual(higher_level(snake_len, level, snake_speed), (2, 10))
+
+    def test_menu(self):
+        choice = pygame.K_q
+        self.assertEqual(menu(choice), (True, False))
